330a8a7109049a8c6149e7e26fbb11e8cb85f3f1
Arman Ala <arman.ala.developer@gmail.com>
2026-08-10 17:22:42 +0330

    refactor(health): use standardized response helper

--- src/api/handlers/health.go
+++ src/api/handlers/health.go
@@ -1,14 +1,21 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/arman-ala/Car_shop/api/helper"
+	"github.com/gin-gonic/gin"
+)
 
 type HealthHandler struct {
 }
 
 func NewHealthHandler() *HealthHandler {
 	return &HealthHandler{}
-} 
+}
 
 func (h *HealthHandler) Health(c *gin.Context) {
-	c.JSON(200, "/health works!")
+	c.JSON(200, helper.GenerateBaseResponse(gin.H{
+		"message": "/health works fine.",
+	}, true,
+		0,
+	))
 }
